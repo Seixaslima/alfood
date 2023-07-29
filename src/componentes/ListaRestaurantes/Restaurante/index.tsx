9971e6106362810ee76db1061490e5404515dab4
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-07-29 18:22:47 -0300

    unidade 1 finalizada

--- src/componentes/ListaRestaurantes/Restaurante/index.tsx
+++ src/componentes/ListaRestaurantes/Restaurante/index.tsx
@@ -1,19 +1,33 @@
+import { useEffect, useState } from 'react';
 import IRestaurante from '../../../interfaces/IRestaurante';
 import Prato from '../Prato';
 import estilos from './Restaurante.module.scss';
+import axios from 'axios';
+import IPrato from '../../../interfaces/IPrato';
 
 interface RestauranteProps {
   restaurante: IRestaurante
 }
 
 const Restaurante = ({ restaurante }: RestauranteProps) => {
+  const linkPratos = `http://localhost:8000/api/v1/restaurantes/${restaurante.id}/pratos/`;
+  const [pratos, setPratos] = useState<IPrato[]>();
+
+
+  useEffect(() => {
+    axios.get(linkPratos)
+      .then(resposta => {
+        setPratos(resposta.data)
+      })
+      .catch(e => console.log(e))
+  }, [linkPratos])
 
   return (<section className={estilos.Restaurante}>
     <div className={estilos.Titulo}>
       <h2>{restaurante.nome}</h2>
     </div>
     <div>
-      {restaurante.pratos?.map(item => <Prato prato={item} key={item.id} />)}
+      {pratos?.map(item => <Prato prato={item} key={item.id} />)}
     </div>
   </section>)
 }
--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,98 +1,41 @@
+import { useEffect, useState } from 'react';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
+import axios from 'axios';
+import { IPaginacao } from '../../interfaces/IPaginacao';
 
 const ListaRestaurantes = () => {
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+  const [proximaPagina, setProximaPagina] = useState("");
 
-  const restaurantes: IRestaurante[] = [
-    {
-      id: 1,
-      nome: "Lyllys Cafe",
-      pratos: [
-        {
-          id: 1,
-          descricao: 'Lasanha à Bolonhesa',
-          imagem: 'https://img.cybercook.com.br/receitas/731/lasanha-3.jpeg',
-          nome: 'Lasanha',
-          restaurante: 1,
-          tag: 'Italiana'
-        },
-        {
-          id: 2,
-          descricao: 'Strogonoff de Frango à brasileira',
-          imagem: 'https://d1uz88p17r663j.cloudfront.net/original/73cfbb5072ff136c81aed2c7c3a7cd65_stogonoff-carne-receitas-nestle.jpg',
-          nome: 'Strogonoff',
-          restaurante: 1,
-          tag: 'Russa'
-        },
-        {
-          id: 3,
-          descricao: 'Empadão de Frango',
-          imagem: 'https://cooknenjoy.com/wp-content/uploads/2021/05/empadao-frango-02-1-1300x976.jpg',
-          nome: 'Empadão de Frango',
-          restaurante: 1,
-          tag: 'Portuguesa'
-        }
-      ]
-    },
-    {
-      id: 2,
-      nome: "Sugiro Sushi",
-      pratos: [
-        {
-          id: 1,
-          descricao: 'Combinado de 8 peças',
-          imagem: 'https://www.sabornamesa.com.br/media/k2/items/cache/5031e263a4a258791d6306b2d3d9dbf6_XL.jpg',
-          nome: 'Sushi',
-          restaurante: 1,
-          tag: 'Japonesa'
-        },
-        {
-          id: 2,
-          descricao: 'Sashimi de Salmão',
-          imagem: 'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2009/04/sashimi_facil.jpg',
-          nome: 'Sashimi',
-          restaurante: 1,
-          tag: 'Japonesa'
-        }
-      ]
-    },
-    {
-      id: 3,
-      nome: "Cantina da Escola",
-      pratos: [
-        {
-          id: 1,
-          descricao: 'Salgado de queijo com presunto',
-          imagem: 'https://mesaesabor.com.br/wp-content/uploads/2021/01/enrolado-de-presunto-e-queijo-848x477.png',
-          nome: 'Quejunto',
-          restaurante: 1,
-          tag: 'Lanche'
-        },
-        {
-          id: 2,
-          descricao: 'Coxinha de Frango',
-          imagem: 'https://t1.rg.ltmcdn.com/pt/posts/1/9/1/coxinha_simples_191_600.jpg',
-          nome: 'Coxinha',
-          restaurante: 1,
-          tag: 'Lanche'
-        },
-        {
-          id: 3,
-          descricao: 'Risole de Palmito',
-          imagem: 'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2012/10/risole_palmitoo.jpg',
-          nome: 'Risole',
-          restaurante: 1,
-          tag: 'Lanche'
-        }
-      ]
-    }
-  ]
+  useEffect(() => {
+    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
+      .then(resposta => {
+        setRestaurantes(resposta.data.results)
+        setProximaPagina(resposta.data.next)
+      })
+      .catch(error => console.log(error))
+  }, [])
 
-  return (<section className={style.ListaRestaurantes}>
-    <h1>Os restaurantes mais <em>bacanas</em>!</h1>
-    {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
-  </section>)
+  function verMais() {
+    axios.get<IPaginacao<IRestaurante>>(proximaPagina)
+      .then(resposta => {
+        setRestaurantes([...restaurantes, ...resposta.data.results])
+        setProximaPagina(resposta.data.next)
+      })
+      .catch(error => console.log(error))
+  }
+
+  return (
+    <section className={style.ListaRestaurantes}>
+      <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+      {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+      {proximaPagina &&
+        <button onClick={verMais}>
+          ver mais
+        </button>}
+    </section>)
 }
 
 export default ListaRestaurantes
